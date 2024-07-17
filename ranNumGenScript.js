617605b7e237b09f78083b178b433c29b5082a95
AtlanticBee <165555169+AtlanticBee@users.noreply.github.com>
2024-07-17 15:23:09 +0000

    update 6

--- ranNumGenScript.js
+++ ranNumGenScript.js
@@ -12,7 +12,7 @@ document.getElementById("goButton").onclick=async() => {
     const balkhashAPIresponse = await fetch('https://api.open-meteo.com/v1/forecast?latitude=46.8481&longitude=74.995&hourly=temperature_2m,wind_speed_10m&past_days=2&forecast_days=1');
     const balkhashJson = await balkhashAPIresponse.json();
     console.log(balkhashJson);
-    let balkhashAverageTemperature = balkhashJson.hourly.temperature_2m.reduce((a, b) => a + b, 0);        //A new function I've learned. Reduce takes two functions: a is the accumulator, b is the default value to add if empty element.
+    let balkhashAverageTemperature = (Math.round((balkhashJson.hourly.temperature_2m.reduce((a, b) => a + b, 0) / 72) * 100)/100).toFixed(2);        //A new function I've learned. Reduce takes two functions: a is the accumulator, b is the default value to add if empty element.
     let balkhashAverageWindspeed = (Math.round((balkhashJson.hourly.wind_speed_10m.reduce((a, b) => a + b, 0) / 72) * 100)/100).toFixed(2);
 
     let milliSeconds = new Date().getMilliseconds();
@@ -29,7 +29,7 @@ document.getElementById("goButton").onclick=async() => {
 
     let randomMultiple = (blackpoolTotalRain + 1) * (blackpoolAverageTemperature + 1) * (balkhashAverageTemperature + 1) * (balkhashAverageWindspeed + 1) * (milliSeconds + 1);
     let sizeOfMultiple = Math.log10(randomMultiple);
-    let randomNumber = (randomMultiple / ((10*sizeOfMultiple) + 1)).toFixed(10);
+    let randomNumber = (randomMultiple / ((10*sizeOfMultiple) + 1));
     console.log(randomNumber);
     let resultParagraph = document.getElementById("result");
     let textAdded = document.createTextNode(randomNumber);
