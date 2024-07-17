43b5a7403757a564e70aea78e84e4615eb443a14
AtlanticBee <165555169+AtlanticBee@users.noreply.github.com>
2024-07-17 14:30:54 +0000

    update 2

--- ranNumGenScript.js
+++ ranNumGenScript.js
@@ -5,11 +5,13 @@ let goButton = document.getElementById("goButton");
 document.getElementById("goButton").onclick=async() => {
     const blackpoolAPIresponse = await fetch('https://api.open-meteo.com/v1/forecast?latitude=53.8167&longitude=-3.05&hourly=temperature_2m,rain&past_days=2&forecast_days=1');
     const blackpoolJson = await blackpoolAPIresponse.json();
+    console.log(blackpoolJson);
     let blackpoolTotalRain = blackpoolJson.hourly.rain.reduce((a, b) => a + b, 0);        //A new function I've learned. Reduce takes two functions: a is the accumulator, b is the default value to add if empty element.
     let blackpoolAverageTemperature = (Math.round((blackpoolJson.hourly.temperature_2m.reduce((a, b) => a + b, 0) / 72) * 100)/100).toFixed(2);
 
     const balkhashAPIresponse = await fetch('https://api.open-meteo.com/v1/forecast?latitude=46.8481&longitude=74.995&hourly=temperature_2m,wind_speed_10m&past_days=2&forecast_days=1');
     const balkhashJson = await balkhashAPIresponse.json();
+    console.log(balkhashJson);
     let balkhashAverageTemperature = balkhashJson.hourly.temperature_2m.reduce((a, b) => a + b, 0);        //A new function I've learned. Reduce takes two functions: a is the accumulator, b is the default value to add if empty element.
     let balkhashAverageWindspeed = (Math.round((balkhashJson.hourly.windSpeed10m.reduce((a, b) => a + b, 0) / 72) * 100)/100).toFixed(2);
 
