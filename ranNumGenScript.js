d7d44c935d750aba1b22cb288f81dabf734545f0
AtlanticBee <165555169+AtlanticBee@users.noreply.github.com>
2024-07-17 14:25:02 +0000

    mods to API work

--- ranNumGenScript.js
+++ ranNumGenScript.js
@@ -3,36 +3,32 @@
 let goButton = document.getElementById("goButton");
 
 document.getElementById("goButton").onclick=async() => {
-    const weatherAPIresponse = await fetch('https://api.open-meteo.com/v1/forecast?latitude=53.8167&longitude=-3.05&hourly=temperature_2m,rain&past_days=2&forecast_days=1');
-    const weatherJson = await weatherAPIresponse.json();
-    let totalRain = weatherJson.hourly.rain.reduce((a, b) => a + b, 0);        //A new function I've learned. Reduce takes two functions: a is the accumulator, b is the default value to add if empty element.
-    let averageTemp = (Math.round((weatherJson.hourly.temperature_2m.reduce((a, b) => a + b, 0) / 72) * 100)/100).toFixed(2);
-    document.getElementById("weather").innerHTML = "Total Rain = " + totalRain + "\n" + "Average Temperature (C) = " + averageTemp;
+    const blackpoolAPIresponse = await fetch('https://api.open-meteo.com/v1/forecast?latitude=53.8167&longitude=-3.05&hourly=temperature_2m,rain&past_days=2&forecast_days=1');
+    const blackpoolJson = await blackpoolAPIresponse.json();
+    let blackpoolTotalRain = blackpoolJson.hourly.rain.reduce((a, b) => a + b, 0);        //A new function I've learned. Reduce takes two functions: a is the accumulator, b is the default value to add if empty element.
+    let blackpoolAverageTemperature = (Math.round((blackpoolJson.hourly.temperature_2m.reduce((a, b) => a + b, 0) / 72) * 100)/100).toFixed(2);
 
-    let seconds = new Date().getSeconds();      //Returns integer between 0 and 59
-    console.log("seconds = " + seconds);
+    const balkhashAPIresponse = await fetch('https://api.open-meteo.com/v1/forecast?latitude=46.8481&longitude=74.995&hourly=temperature_2m,wind_speed_10m&past_days=2&forecast_days=1');
+    const balkhashJson = await balkhashAPIresponse.json();
+    let balkhashAverageTemperature = balkhashJson.hourly.temperature_2m.reduce((a, b) => a + b, 0);        //A new function I've learned. Reduce takes two functions: a is the accumulator, b is the default value to add if empty element.
+    let balkhashAverageWindspeed = (Math.round((balkhashJson.hourly.windSpeed10m.reduce((a, b) => a + b, 0) / 72) * 100)/100).toFixed(2);
 
-    let artworkInteger = Math.abs(((Math.round((weatherJson.hourly.temperature_2m.reduce((a, b) => a + b, 0) / 72) * 100)/100).toFixed(0) * 10)) - seconds;
-    let artworkAPIrequest = "https://api.artic.edu/api/v1/artworks/" + artworkInteger;
-    const artworkAPIresponse = await fetch(artworkAPIrequest);
-    const artworkData = await artworkAPIresponse.json();
+    let milliSeconds = new Date().getMilliseconds();
 
-    let artist;
-    if (artworkData.artist_title === String){
-        console.log(artworkData.data.artist_title);
-        artist = artworkData.data.artist_title;
-    }
-    else{
-        artist = "not found";
-    }
-    document.getElementById("artwork").innerHTML = "Artist = " + artist;
-    
-    let sumAscii;
-    for (let counter; counter < artist.length; counter++){
-        let charCode = artist.charCodeAt[counter];
-        sumAscii = sumAscii + charCode;
-    }
+    document.getElementById("blackpool").innerHTML = (
+        "Blackpool Total Rain= " + blackpoolTotalRain +
+        "\n" + "Blackpool Average Temperature (C) = " + blackpoolAverageTemperature
+    );
 
-    let randomNumber = Math.round(sumAscii + seconds + averageTemp + totalRain);
+    document.getElementById("balkhash").innerHTML = (
+        "Balkhash Average Windspeed km/h = " + balkhashAverageWindspeed +
+        "\n" + "Balkhash Average Temperature (C) = " + balkhashAverageTemperature
+    );
+
+    let randomMultiple = (blackpoolTotalRain + 1) * (blackpoolAverageTemperature + 1) * (balkhashAverageTemperature + 1) * (balkhashAverageWindspeed + 1) * (milliSeconds + 1);
+    let sizeOfMultiple = Math.log10(randomMultiple);
+    let randomNumber = randomMultiple / ((10*sizeOfMultiple) + 1);
     document.getElementById("result").innerHTML = randomNumber;
   };
+
+  
